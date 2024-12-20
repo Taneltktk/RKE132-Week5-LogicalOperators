779aed4254fde50e3ff6b5b33288ced4bba3d62c
TanelP <tanel.pool@tktk.ee>
2024-12-20 15:57:42 +0200

    add week 5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//AND - && see on väga range tingimusvorm. Kõik tingimused peavad olema täidetud true jaoks,muidu false
+
+//true && true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes (altgr + ><))
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
